e46f6c4262bcd016dfec6035edc03b5f6533842b
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2021-08-22 12:07:54 -0400

    implement filter functionality and styling

--- connectnowapp/src/Components/NavBar.js
+++ connectnowapp/src/Components/NavBar.js
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
+import "../Styles/NavBar.css"
+import logo from "./Images/logo.jpeg"
 
 const NavBar = () => {
   return (
-    <nav>
-      <NavLink to="/">Get Connected to All</NavLink>
-      <NavLink to="/aboutus">About Us</NavLink>
+    <nav className="nav-div">
+      <NavLink to="/"><img className="logo-image" src={logo}></img></NavLink>
+      <NavLink style={{ textDecoration: 'none' }} to="/"><h1 className="about-us">Get Connected for All</h1></NavLink>
+      <NavLink style={{ textDecoration: 'none' }} to="/aboutus"><h1 className="about-us">About Us</h1></NavLink>
     </nav>
   );
 };
